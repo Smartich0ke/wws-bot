30069cec36e5edb80036caf4016553ba7485f64e
Smartich0ke <nikolaipatricknp@gmail.com>
2022-11-11 15:25:48 +1030

    fix bug that made the bot fail to run commands

--- index.js
+++ index.js
@@ -21,7 +21,12 @@ const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
 	const command = require(filePath);
-	client.commands.set(command.name, command);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
 }
 
 client.once(Events.ClientReady, () => {
